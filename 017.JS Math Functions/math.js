1fa4ef58b65b30dfba6e73d226daeee7b18e2f88
mdmarufsarker <mdmarufsarker2021@gmail.com>
2024-01-11 12:20:01 +0600

    folders renamed

---
+++ 017.JS Math Functions/math.js
@@ -0,0 +1,14 @@
+console.log(Math.E) // 2.718281828459045
+console.log(Math.PI) // 3.141592653589793
+let num = 4.589;
+console.log(Math.abs(num)); // 4.589
+console.log(Math.floor(num)); // 4
+console.log(Math.ceil(num)); // 5
+console.log(Math.round(num)); // 5
+console.log(Math.max(400, 500, 600)); // 600
+console.log(Math.min(400, 500, 600)); // 400
+console.log(Math.pow(2, 3)); // 8
+console.log(Math.pow(3, 5)); // 243
+console.log(Math.sqrt(9)); // 3
+console.log(Math.sqrt(64)); // 8
+console.log(Math.round(Math.random() * 50 + 1)); // 25
---
+++ 019.JS Comments/comments.js
@@ -0,0 +1,15 @@
+let name = "Md. Maruf Sarker";
+let age = 23;
+let department = "Computer Science & Engineering";
+let varsity = "Green University of Bangladesh";
+
+/*
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Department: " + department);
+console.log("University: " + varsity);
+*/
+
+// Ctrl + / to comment out multiple lines
+
+console.log(`${name} is ${age} years old. He is studying ${department} at ${varsity}.`)
